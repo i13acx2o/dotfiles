5438eb18015ccf0085c6760cb9f1b0f5d83c0a57
i13acx2o <i13acx2o@gmail.com>
2025-10-28 02:57:04 +0300

    feat(shell): Added zsh config and setup script

---
+++ shell.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Configure [Z]sh
+mkdir -p ~/.config/zsh
+ln -s ~/Data/Misc/dofiles/zsh/dot-zshenv ~/.zshenv
+ln -s ~/Data/Misc/dofiles/zsh/dot-zshrc ~/.config/zsh/.zshrc
+ln -s ~/Data/Misc/dofiles/zsh/dot-zprofile ~/.config/zsh/.zprofile
