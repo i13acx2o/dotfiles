e0edcd86ca9aa7bbb9449387547986ab5fe03ffc
i13acx2o <i13acx2o@icloud.com>
2026-08-25 03:45:13 +0300

    feat: neovim

---
+++ nvim/after/lsp/gopls.lua
@@ -0,0 +1,17 @@
+return {
+  cmd = { "gopls" },
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      hints = {
+        rangeVariableTypes = true,
+        parameterNames = true,
+        constantValues = true,
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        functionTypeParameters = true,
+      },
+    },
+  },
+}
